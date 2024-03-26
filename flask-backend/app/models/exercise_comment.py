87a15032b5852b5e3868f5f3d8fb3972d09ce12f
Khobe Arzadon <khobearzadon@yahoo.com>
2024-03-26 12:44:43 -0700

    added some models

--- flask-backend/app/models/exercise_comment.py
+++ flask-backend/app/models/exercise_comment.py
@@ -8,3 +8,21 @@ class Exercise_Comment(db.Model):
     __tablename__ = "exercise_comments"
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
+
+    id=db.Column(db.Integer, primary_key=True)
+    userId=db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")))
+    exerciseId=db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("exercises.id"), ondelete="CASCADE"))
+    description=db.Column(db.Text, nullable=False)
+    createdAt = db.Column(db.Date, default=dt.datetime.now())
+    updatedAt = db.Column(db.Date, default=dt.datetime.now())
+
+    exercise = db.relationship("Exercise", back_populates = "exercise_comments")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'userId': self.userId,
+            'exerciseId': self.exerciseId,
+            'description': self.description,
+            'createdAt': str(self.createdAt)
+        }
