498b270bc7d3eed4d069818e6e929d822d557f53
Khobe Arzadon <khobearzadon@yahoo.com>
2024-03-26 13:14:58 -0700

    ran build

---
+++ flask-backend/app/seeds/seed_data.py
@@ -0,0 +1,124 @@
+users= [
+
+    {
+        "firstName": "Mike",
+        "lastName": "Wazowski",
+        "email": "googlybear@hotmail.com",
+        "password": "scarymonster564",
+    },
+    {
+        "firstName": "James",
+        "lastName": "Sullivan",
+        "email": "boomonster@gmail.com",
+        "password": "purpledotsonme32",
+    },
+    {
+        "firstName": "Donald",
+        "lastName": "Duck",
+        "email": "quackquack@yahoo.com",
+        "password": "ilovedaisy45",
+    },
+    {
+        "firstName": "Daisy",
+        "lastName": "Duck",
+        "email": "ballerina@hotmail.com",
+        "password": "daisyismyname278",
+    },
+    {
+        "firstName": "Peter",
+        "lastName": "Pan",
+        "email": "neverland@gmail.com",
+        "password": "captainhookiscrazy13",
+    },
+    {
+        "firstName": "Bud",
+        "lastName": "Robinson",
+        "email": "inventions@yahoo.com",
+        "password": "tothefuture732",
+    },
+    {
+        "firstName": "Toby",
+        "lastName": "Turtle",
+        "email": "journeysawait@hotmail.com",
+        "password": "slowwinstherace27",
+    },
+    {
+        "firstName": "Jack",
+        "lastName": "Sparrow",
+        "email": "richesgalore@gmail.com",
+        "password": "iamapirate68",
+    },
+    {
+        "firstName": "Buzz",
+        "lastName": "Lightyear",
+        "email": "toinfinity@yahoo.com",
+        "password": "beyondspace694",
+    },
+    {
+        "firstName": "Luke",
+        "lastName": "Skywalker",
+        "email": "theforce@hotmail.com",
+        "password": "r2d23cpo",
+    },
+    {
+        "firstName": "Han",
+        "lastName": "Solo",
+        "email": "memyselfandi@gmail.com",
+        "password": "iloveleia678",
+    },
+    {
+        "firstName": "Mace",
+        "lastName": "Windu",
+        "email": "purplesaber@yahoo.com",
+        "password": "republic532",
+    },
+    {
+        "firstName": "Ahsoka",
+        "lastName": "Tano",
+        "email": "thebestpadawan@hotmail.com",
+        "password": "goodoverevil832",
+    },
+    {
+        "firstName": "Doc",
+        "lastName": "Hudson",
+        "email": "oldcar@gmail.com",
+        "password": "iloveracing947",
+    },
+    {
+        "firstName": "Carl",
+        "lastName": "Fredricksen",
+        "email": "oldman@yahoo.com",
+        "password": "traveltheworld453",
+    },
+    {
+        "firstName": "Charles",
+        "lastName": "Muntz",
+        "email": "travelerofeverywhere@hotmail.com",
+        "password": "paradisefalls238",
+    },
+    {
+        "firstName": "Mei",
+        "lastName": "Lee",
+        "email": "redpanda@gmail.com",
+        "password": "fourtown4life",
+    },
+    {
+        "firstName": "Barley",
+        "lastName": "Lightfoot",
+        "email": "wizardmaster@yahoo.com",
+        "password": "wheresdad452",
+    },
+    {
+        "firstName": "John",
+        "lastName": "White",
+        "email": "jwhite@gmail.com",
+        "password": "jwpassword12",
+    },
+    {
+        "firstName": "Jane",
+        "lastName": "Wallen",
+        "email": "wallenj@yahoo.com",
+        "password": "foodlover243",
+    }
+
+]
--- flask-backend/app/seeds/users.py
+++ flask-backend/app/seeds/users.py
@@ -1,19 +1,13 @@
 from app.models import db, User, environment, SCHEMA
 from sqlalchemy.sql import text
+from .seed_data import users
 
 
 # Adds a demo user, you can add other users here if you want
 def seed_users():
-    demo = User(
-        username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
-
-    db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    for user in users:
+        newRes = User(**user)
+        db.session.add(newRes)
     db.session.commit()
 
 
@@ -28,5 +22,5 @@ def undo_users():
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM users"))
-        
+
     db.session.commit()
