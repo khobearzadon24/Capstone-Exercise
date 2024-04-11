62113b682d0f608e18ea009191cd4eb75877b4ae
Khobe Arzadon <khobearzadon@yahoo.com>
2024-04-10 22:36:38 -0700

    added footers

---
+++ react-vite/src/components/Footer/Footer.jsx
@@ -0,0 +1,24 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import "./Footer.css";
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div>
+        Khobe
+        <Link to="https://github.com/khobearzadon24" target="_blank">
+          <FaGithub />
+        </Link>
+        <Link
+          to="https://www.linkedin.com/in/khobe-arzadon-4b985a202/"
+          target="_blank"
+        >
+          <FaLinkedin />
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
